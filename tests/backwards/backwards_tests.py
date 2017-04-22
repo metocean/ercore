8a52043b749bde6449c3673a4ee983980874ead0
Rosa <rosatrancoso@gmail.com>
2017-04-22 15:23:43 +1200

    add backwards in time tests

--- tests/backwards/backwards_tests.py
+++ tests/backwards/backwards_tests.py
@@ -28,22 +28,24 @@ def create_gridded_current (x,y,u,v,time,
                             units = 'days since 1-1-1', 
                             filecurr='cur.nc'):
     import netCDF4
-    u = u.astype('float32')
-    v = v.astype('float32')
+    u = u.astype('float64')
+    v = v.astype('float64')
+    time = netCDF4.date2num(time, units)
+    time = time.astype('float64')
     print 'Creating %s' % filecurr
     nc = netCDF4.Dataset(filecurr,'w')
     nc.createDimension('lon', len(x))
     nc.createDimension('lat', len(y))
     nc.createDimension('time', len(time))
-    nc.createVariable('lon','float32',('lon'))
-    nc.createVariable('lat','float32',('lat'))
-    t = nc.createVariable('time','float32',('time'))
+    nc.createVariable('lon','float64',('lon'))
+    nc.createVariable('lat','float64',('lat'))
+    t = nc.createVariable('time','float64',('time'))
     t.units = units
     nc.createVariable('u','float32',('time', 'lat','lon'))
     nc.createVariable('v','float32',('time', 'lat','lon'))
     nc.variables['lon'][:] = x
     nc.variables['lat'][:] = y
-    nc.variables['time'][:] = [netCDF4.date2num(t1, units) for t1 in time]
+    nc.variables['time'][:] = time
     nc.variables['u'][:] = u
     nc.variables['v'][:] = v
     nc.close()
@@ -64,7 +66,7 @@ def create_tide_current(tstart, tend, dt=3600,
     u = np.zeros((nt, len(x),len(y)))
     v = np.zeros(u.shape)
 
-    ut = 0.5/3600. * np.sin(np.linspace(0,2.*np.pi,nt))
+    ut = 0.5/3600.*np.sin(np.linspace(0,2.*np.pi,nt))
     for it in range(nt): u[it,...] = ut[it]
 
     dtimes = [tstart+datetime.timedelta(seconds=t) for t in times]
@@ -78,8 +80,8 @@ def create_tide_current(tstart, tend, dt=3600,
 class TestCase(unittest.TestCase):
 
     def setUp(self):
-        self.tstart = datetime.datetime(2000,1,5)
-        self.tend   = datetime.datetime(2000,1,1)
+        self.tstart = datetime.datetime(2000,1,1)
+        self.tend   = datetime.datetime(2000,1,5)
         self.dt     = -3600 if self.tstart > self.tend else 3600
 
     def tearDown(self):
@@ -181,22 +183,23 @@ class TestCase(unittest.TestCase):
         filecurr = 'tide_cur.nc'
         if not os.path.exists(filecurr):
             print 'Creating tide current file'
-            create_tide_current(tstart=min(self.tstart,self.tend), 
-                                tend=max(self.tstart,self.tend), 
+            create_tide_current(tstart=min(self.tstart,self.tend)-datetime.timedelta(days=1), 
+                                tend=max(self.tstart,self.tend)+datetime.timedelta(days=1), 
                                 filecurr=filecurr)
 
         current=GriddedMover('cur',['u','v'],file=filecurr)
         p1 = PassiveTracer( id=imp,
                             outfile='%s.out' % imp,
                             movers=[current], 
-                            reln=100,
+                            reln=10,
                             nbuff=1000, 
                             P0=[0,0,0],
                             tstart = self.tstart,
-                            tend =   self.tend)
+                            tend =   self.tstart)
 
         ercore=ERcore(geod=False)
         ercore.materials=[p1]
+        # import pdb;pdb.set_trace()
         ercore.run(t=self.tstart,tend=self.tend,dt=self.dt)
 
         df = read_release_txt('%s.out' % imp)
@@ -217,6 +220,50 @@ class TestCase(unittest.TestCase):
                     P0 = [0,0,0])
 
 
+    def test_tide_polygon (self):
+        imp = 'tide_poly'
+        if self.dt < 0: imp += '_back'
+        print 'Running for imp %s' % imp
+
+        filecurr = 'tide_cur.nc'
+        if not os.path.exists(filecurr):
+            print 'Creating tide current file'
+            create_tide_current(tstart=min(self.tstart,self.tend)-datetime.timedelta(days=1), 
+                                tend=max(self.tstart,self.tend)+datetime.timedelta(days=1), 
+                                filecurr=filecurr)
+
+        current=GriddedMover('cur',['u','v'],file=filecurr)
+
+        p1 = PassiveTracer( id=imp,
+                            outfile='%s.out' % imp,
+                            movers=[current], 
+                            reln=100,
+                            nbuff=1000, 
+                            polygon=[[0,0],[0,0.01],[0.01,1],[0,1]],
+                            tstart = self.tstart,
+                            tend =   self.tend)
+
+        ercore=ERcore(geod=False)
+        ercore.materials=[p1]
+        ercore.run(t=self.tstart,tend=self.tend,dt=self.dt)
+
+        df = read_release_txt('%s.out' % imp)
+        print df.head()
+
+        plot_all(df = df,
+                colors=['b', 'r', 'g', 'k'],
+                # lims=[-15,1,0,2],
+                fileplotpref='plt_%s' % imp,
+                polygon = [[0,0],[0,0.01],[0.01,1],[0,1]],
+                dens = True)
+
+
+        plot_frames(df= df,
+                    usemap=False,
+                    lims=None,
+                    itout=12,
+                    fileplotpref='plt_%s' % imp,
+                    polygon = [[0,0],[0,0.01],[0.01,1],[0,1]])
 
 
 if __name__ == '__main__':
